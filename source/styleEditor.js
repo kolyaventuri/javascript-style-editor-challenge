acb52fa337b7ff7a1f845447a4136a1be1eac6c4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-23 12:39:04 -0600

    Adds functions to handle form submit, and send to a setProperty() function that updates the DOM

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit((e) => {
+    e.preventDefault();
+
+    let selector = $('#style_editor input[name="selector"]').val();
+    let property = $('#style_editor input[name="property"]').val();
+    let value    = $('#style_editor input[name="value"]').val();
+
+    setProperty(selector, property, value);
+  });
 });
+
+const setProperty = (selector, property, value) => {
+  if(!selector || !property || !value) throw new Error('Invalid arguments.');
+
+  $(selector).css(property, value);
+};
